594d9461f17274303e924739dd4062d59fbd9b0a
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-01-15 13:28:26 +0800

    Adding code

--- rating-react-app-2/src/Components/Thankyou.jsx
+++ rating-react-app-2/src/Components/Thankyou.jsx
@@ -4,7 +4,7 @@ export default function Thankyou({editnumbers, setEditButtons, thanksyours}){
     return(
       <div className='thank-you'>
           <img src= {thankyou} alt="thankyou" />
-          <p className='p-1'>You selected {editnumbers} {!editnumbers ?"0": undefined} out of 5</p>
+          <p className='p-1'>You selected {editnumbers} {!editnumbers ? "0": undefined} out of 5</p>
           <h2>{thanksyours}</h2>
           <p className='p-2'>We appreciate you taking the time to give a rating. If you ever need
           more support, don't hesitate to get in touch!</p>
