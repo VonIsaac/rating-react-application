7f24118fc5efed333c493d61ce03b78eed42454c
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-01-17 00:15:51 +0800

    lifting state and new jsx file

--- rating-react-app-2/src/App.jsx
+++ rating-react-app-2/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Thankyou from './Components/Thankyou';
+import Button from './Components/Button.jsx';
 import { useState } from 'react';
 
 
@@ -20,40 +20,16 @@ function App() {
     setButton(editbutton => !editbutton)
   }
 
-  function Button({number}){
-    return(
-          <>
-             <button className='main-btn' onClick={ () => setEditNumber(number)}>{number}</button>
-
-             
-          </>
-    )
-}
+ 
 
   return (
     <>
-      {!isButton && (
-        <div className='wrapper'>
-          <h2>How did we do?</h2>
-        <p> Please let us know how we did with your support request. All
-          feedback is appreciated to help us improve our offering!</p>
-
-          <div className='btns'>
-            
-              <Button number={1} />
-              <Button number={2} />
-              <Button number={3} />
-              <Button number={4} />
-              <Button number={5} />
-          </div>
-
-          <div className='btn-sub'>
-                <button onClick={ setEditButton}>Submit</button>
-            </div>
-    </div>
-      )}
-
-      {isButton && <Thankyou setEditButtons = { setEditButton} editnumbers = {isNumber}  thanksyours= "Thank you!"/>}
+      <Button
+        isButtons = {isButton}
+        isNumbers = {isNumber}
+        setNumberEdit ={setEditNumber}
+        setButtonEdit = {setEditButton}
+      />
     </>
   )
 
---
+++ rating-react-app-2/src/Components/Button.jsx
@@ -0,0 +1,41 @@
+import Thankyou from "../Components/Thankyou.jsx"
+
+export default function Button({ isButtons, isNumbers, setNumberEdit, setButtonEdit}){
+
+    function Button({number}){
+        return(
+              <>
+                 <button className='main-btn' onClick={ () => setNumberEdit(number)}>{number}</button>
+    
+                 
+              </>
+        )
+    }
+
+    return(
+        <>
+        {!isButtons && (
+          <div className='wrapper'>
+            <h2>How did we do?</h2>
+          <p> Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!</p>
+  
+            <div className='btns'>
+              
+                <Button number={1} />
+                <Button number={2} />
+                <Button number={3} />
+                <Button number={4} />
+                <Button number={5} />
+            </div>
+  
+            <div className='btn-sub'>
+                  <button onClick={ setButtonEdit}>Submit</button>
+              </div>
+      </div>
+        )}
+  
+        {isButtons && <Thankyou setEditButtons = {setButtonEdit} editnumbers = {isNumbers}  thanksyours= "Thank you!"/>}
+      </>
+    )
+}
